2a0c1b1afed3d0b6b7be4dac7cd7cf662026ef04
Naeem <naeemqurashii674@gmail.com>
2026-03-12 03:41:48 +0500

    everything is perfect in comment section

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -13,7 +13,8 @@ import moment from "moment";
 import "./PlayVideo.css";
 const PlayVideo = ({ videoId }) => {
   const [apiData, setApiData] = useState(null);
-  const [channelData,setChannelData]=useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commnetData, setCommentData] = useState([]);
 
   const fetchVideoData = async () => {
     const videoDetails_url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${API_KEY}`;
@@ -23,19 +24,23 @@ const PlayVideo = ({ videoId }) => {
   };
 
 
-    const fetchOtherData = async () =>{
-      // Fetch Channel Data 
-      const channelData_url=`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
-      await fetch(channelData_url)
-      .then(res=>res.json())
-      .then(data=>setChannelData(data.items[0]))
-    }
+  const fetchOtherData = async () => {
+    // Fetch Channel Data 
+    const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+    await fetch(channelData_url)
+      .then(res => res.json())
+      .then(data => setChannelData(data.items[0]))
+
+    // Fetching commnet data
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`
+    await fetch(comment_url).then(res => res.json()).then(data => setCommentData(data.items))
+  }
   useEffect(() => {
     fetchVideoData();
   }, []);
-  useEffect(()=>{
+  useEffect(() => {
     fetchOtherData();
-  },[apiData])
+  }, [apiData])
 
   return (
     <div className="play-video">
@@ -47,14 +52,14 @@ const PlayVideo = ({ videoId }) => {
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
       ></iframe>
-     
+
       <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
       <div className="play-video-info">
-        <p>{apiData?value_converter(apiData.statistics.viewCount):"16k"} Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
+        <p>{apiData ? value_converter(apiData.statistics.viewCount) : "16k"} Views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : ""}</p>
         <div>
           <span>
             <img src={like} alt="" />
-           {apiData?value_converter (apiData.statistics.likeCount) :155}
+            {apiData ? value_converter(apiData.statistics.likeCount) : 155}
           </span>
           <span>
             <img src={dislike} alt="" />
@@ -71,107 +76,40 @@ const PlayVideo = ({ videoId }) => {
       </div>
       <hr />
       <div className="publisher">
-        <img src={channelData?channelData.snippet.thumbnails.default.url:""} alt="" />
+        <img src={channelData ? channelData.snippet.thumbnails.default.url : ""} alt="" />
         <div>
-          <p>{apiData?apiData.snippet.channelTitle:""}</p>
-          <span>{channelData?value_converter(channelData.statistics.subscriberCount):"1M"} Subscriber </span>
+          <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
+          <span>{channelData ? value_converter(channelData.statistics.subscriberCount) : "1M"} Subscriber </span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-discription">
-        <p>{apiData?apiData.snippet.description.slice(0,250):"Discription Here"}</p>
+        <p>{apiData ? apiData.snippet.description.slice(0, 250) : "Discription Here"}</p>
         <hr />
-        <h4>{apiData? value_converter(apiData.statistics.commentCount):102} Comments</h4>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              A global computer network providing a variety of information and
-              communocation for interconnected networks using standardlized
-              communication protocols
-              <div className="comment-action ">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
+        <h4>{apiData ? value_converter(apiData.statistics.commentCount) : 102} Comments</h4>
+
+        {commnetData.map((item, index) => {
+          return (
+            <div key={index} className="comment">
+              <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+              <div>
+                <h3>
+                     {item.snippet.topLevelComment.snippet.authorDisplayName} <span>1 day ago</span>
+                </h3>
+                <p>
+                {item.snippet.topLevelComment.snippet.textDisplay}
+                  <div className="comment-action ">
+                    <img src={like} alt="" />
+                    <span>{ value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                    <img src={dislike} alt="" />
+                  </div>
+                </p>
               </div>
-            </p>
-          </div>
-        </div>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              A global computer network providing a variety of information and
-              communocation for interconnected networks using standardlized
-              communication protocols
-              <div className="comment-action ">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-            </p>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              A global computer network providing a variety of information and
-              communocation for interconnected networks using standardlized
-              communication protocols
-              <div className="comment-action ">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-            </p>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              A global computer network providing a variety of information and
-              communocation for interconnected networks using standardlized
-              communication protocols
-              <div className="comment-action ">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-            </p>
-          </div>
-        </div>{" "}
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              A global computer network providing a variety of information and
-              communocation for interconnected networks using standardlized
-              communication protocols
-              <div className="comment-action ">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-            </p>
-          </div>
-        </div>
+            </div>
+          )
+        })}
+
+
       </div>
     </div>
   );
