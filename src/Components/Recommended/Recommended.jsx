289f6b434153f56deb91850b3834fede6f405b75
Naeem <naeemqurashii674@gmail.com>
2026-03-14 01:12:31 +0500

    fixed the recommended section

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { API_KEY } from "../../data";
+
 import "./Recommended.css";
 import thumbnail1 from "../../assets/thumbnail1.png";
 import thumbnail2 from "../../assets/thumbnail2.png";
@@ -9,75 +11,160 @@ import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
 
-const Recommended = () => {
+const Recommended = ({ categoryId }) => {
+  const [apiData, setApiData] = React.useState([]);
+
+  const fetchData = async () => {
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+    await fetch(relatedVideo_url).then(res => res.json()).then(data => setApiData(data.items));
+  }
+  console.log(fetchData);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div className="recommended">
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
+   
+        <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
         </div>
-      </div>
-    
-      <div className="side-video-list">
-        <img src={thumbnail2} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
+         <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
+        </div> <div  className="side-video-list">
+          <img src={thumbnail1} alt="" />
+          <div className="vid-info">
+            <h4>Best channel that help you to be a web developer</h4>
+            <p>Naeem Abbas</p>
+            <p>199k views</p>
+          </div>
         </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail3} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail4} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail5} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail6} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail7} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
-        </div>
-      </div>
-       <div className="side-video-list">
-        <img src={thumbnail8} alt="" />
-        <div className="vid-info">
-          <h4>Best channel that help you to be a web developer</h4>
-          <p>Naeem Abbas</p>
-          <p>199k views</p>
-        </div>
-      </div>
+     
     </div>
+
   );
 };
 
